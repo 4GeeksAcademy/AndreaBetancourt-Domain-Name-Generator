a6fa800ecb0628fa7ee7888d6cd096831e360bce
Andrea Betancourt <111385731+AndreaBetan@users.noreply.github.com>
2023-06-17 15:40:43 +0000

    final

--- src/app.js
+++ src/app.js
@@ -8,19 +8,39 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
 
-  var pronoun = ["the", "our"];
-  var adj = ["great", "big"];
-  var noun = ["jogger", "racoon"];
+  function generateDomain(pronoun, adj, noun, ext) {
+    let result = [];
 
-  for (const i in pronoun) {
-    for (const j in adj) {
-      for (const k in noun) {
-        const pronombre = pronoun[i];
-        const adjetivo = adj[j];
-        const sustantivo = noun[k];
+    for (const i in pronoun) {
+      for (const j in adj) {
+        for (const k in noun) {
+          for (const l in ext) {
+            const pronombre = pronoun[i];
+            const adjetivo = adj[j];
+            const sustantivo = noun[k];
+            const extension = ext[l];
 
-        console.log(pronombre + adjetivo + sustantivo + "");
+            let domain = pronombre + adjetivo + sustantivo + extension;
+
+            result.push(domain);
+          }
+        }
       }
     }
+
+    return result;
   }
+
+  let pronombres = ["the", "our"];
+  let adjetivos = ["great", "big"];
+  let sustantivos = ["jogger", "racoon"];
+  let extensiones = [".com", ".net", ".us", ".io"];
+
+  let domains = generateDomain(pronombres, adjetivos, sustantivos, extensiones);
+
+  console.log(domains);
+
+  document.querySelector("#domain").innerHTML = domains
+    .map(domain => '<li class="list-group-item">' + domain + "</li>")
+    .join("");
 };
